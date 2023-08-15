e53ec10ad63e02f44f4dfa09f8af40ed9a2a562c
Ali Danish <ali.danish@jugnu.pk>
2023-08-15 17:37:16 +0500

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,142 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Ali Danish",
+  role: "Frontend Developer React/React Native",
+  linkedinId: "www.linkedin.com/in/ali-danish-dev",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, develop sites on WordPress or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/ali-danish-dev/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/alidanish007",
+      className: "fa fa-github",
+    },
+    {
+      name: "gmail",
+      url: "mailto:alidani0900@gmail.com",
+      className: "fa fa-envelope",
+    },
+  ],
+  aboutme:
+    "With 1.5 years of experience, I’ve developed mobile and web applications for high-volume businesses. Expert in CSS, JavaScript, React Native, ReactJs, WordPress, and I deliver innovative solutions on time. Strong problem-solving skills, multitasking abilities, and effective workload prioritization. Excellent communication and a reliable team player who excels in collaborative environments.",
+  address: "Samnabad,Lahore",
+  website: "https://github.com/alidanish007",
+  education: [
+    {
+      UniversityName: "University Of Central Punjab",
+      specialization: "",
+      MonthOfPassing: "July",
+      YearOfPassing: "2022",
+      Achievements: "",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Jugnu",
+      specialization: "React Native",
+      MonthOfLeaving: "Present",
+      YearOfLeaving: "",
+      Achievements: "Complete Revamp Of Jugnu Retailer App 2.0 launch",
+    },
+    {
+      CompanyName: "Developers Alley",
+      specialization: "ReactJs",
+      MonthOfLeaving: "May",
+      YearOfLeaving: "2022",
+      Achievements:
+        "Collaborate with the design team to translate visual designs into high-quality code.",
+    },
+    {
+      CompanyName: "Nishat Chunain Limited",
+      specialization: "Mis Intern",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements:
+        "Support from the help desk, administration, maintenance of the hardware inventory, deployment and configuration of networking devices, and documentation of configuration modification.",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "ReactNative",
+    },
+    {
+      skillname: "Javascript",
+    },
+    {
+      skillname: "WordPress",
+    },
+  ],
+  portfolio: [
+    {
+      title: "React Native",
+      name: "J-Retailer",
+      description: "Mobile App",
+      imgurl: "./images/portfolio/P1.jpeg",
+      link: "",
+      about:
+        "Jugnu App is a B2B ordering application designed exclusively for retailers.",
+    },
+    {
+      title: "React Native",
+      name: "Attornea",
+      description: "Mobile App",
+      imgurl: "images/portfolio/P2.png",
+      link: "",
+      about:
+        "Attornea is the mobile application for hiring Lawyers and onilne store.",
+    },
+    {
+      title: "ReactJs",
+      name: "Indigo Apartments",
+      description: "WebApp",
+      imgurl: "images/portfolio/P3.png",
+      link: "",
+      about:
+        "Indigo Signature Apartments is web application for booking Residential Suits",
+    },
+    {
+      title: "WordPress",
+      name: "Developers Alley",
+      description: "Website",
+      imgurl: "images/portfolio/P4.png",
+      link: "",
+      about:
+        "Developers Alley is company portfolio website build using Elementor/ElemetorPro ",
+    },
+  ],
+  testimonials: [
+    {
+      description:
+        "Highly skilled and professional developer - exceeded all expectations and delivered an outstanding product",
+      name: "Brendon Lyons",
+    },
+    {
+      description:
+        "An exceptional developer who turned our vision into reality – our project wouldn't have been the same without them.",
+      name: "Mr.Hamza ",
+    },
+    {
+      description:
+        "Outstanding work and a pleasure to collaborate with - our project soared to new heights with their expertise.",
+      name: "Shaen Paul",
+    },
+  ],
+};
+
+export default resumeData;
