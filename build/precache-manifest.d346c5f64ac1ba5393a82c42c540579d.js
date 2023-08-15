43c9a84b18de2cb2daa5d26758a1ed9ed789147a
Ali Danish <ali.danish@jugnu.pk>
2023-08-15 17:44:21 +0500

    generated build

---
+++ build/precache-manifest.d346c5f64ac1ba5393a82c42c540579d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c2eb66b9f10e0e4f59e",
+    "url": "/static/css/main.a48cbf14.chunk.css"
+  },
+  {
+    "revision": "2c2eb66b9f10e0e4f59e",
+    "url": "/static/js/main.2c2eb66b.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c3197ff8923870c3afc44e5775f5f57a",
+    "url": "/index.html"
+  }
+];
